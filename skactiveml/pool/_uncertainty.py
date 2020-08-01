01b57bd248784ce00ecb3aa0d9625b09c0c99f30
Pascal Mergard <uk067809@student.uni-kassel.de>
2020-08-01 11:40:38 +0200

    Add comments

--- skactiveml/pool/_uncertainty.py
+++ skactiveml/pool/_uncertainty.py
@@ -7,13 +7,35 @@ import warnings
 
 
 class UncertaintySampling(PoolBasedQueryStrategy):
-    # TODO: @PM: add comments and doc_string (incl paper reference) as in sklearn @PM: if you copy functions
-    # from modAL, please make clear where they come from (check with licence, if this is okay) hallos
+    """
+    Uncertainty Sampling query stratagy.
+    The implementation is based on http://www.burrsettles.com/pub/settles.activelearning.pdf
 
+    Parameters
+    ----------
+    clf : sklearn classifier
+        A probabilistic sklearn classifier.
+    method : string
+        The method to calculate the uncertainty, entropy, least confident and margin_sampling are possible.
+    random_state : int
+        The random seed to use.
+
+    Attributes
+    ----------
+    method : string
+        The method to calculate the uncertainty, entropy, least confident and margin_sampling are possible.
+    clf : sklearn classifier
+        A probabilistic sklearn classifier.
+
+    Methods
+    -------
+    query(X_cand, X, y, return_utilities=False, **kwargs)
+        Represent the photo in the given colorspace.
+    """
     def __init__(self, clf, method='entropy', random_state=None):
         super().__init__(random_state=random_state)
 
-        if method is not 'entropy' or method is not 'least_confident' or method is not 'margin_sampling':
+        if method != 'entropy' and method != 'least_confident' and method != 'margin_sampling':
             warnings.warn('The method \'' + method + '\' does not exist, \'entropy\' will be used.')
             method = 'entropy'
 
@@ -21,25 +43,49 @@ class UncertaintySampling(PoolBasedQueryStrategy):
         self.clf = clf
 
     def query(self, X_cand, X, y, return_utilities=False, **kwargs):
+        """
+        Queries the next instance to be labeled.
+
+        Parameters
+        ----------
+        X_cand : np.ndarray
+            The unlabeled pool from which to choose.
+        X : np.ndarray
+            The labeled pool used to fit the classifier.
+        y : np.array
+            The labels of the labeled pool X.
+        return_utilities : bool
+            If True, the utilities are returned.
+
+        Returns
+        -------
+        np.ndarray (shape=1)
+            The index of the queried instance.
+        np.ndarray  (shape=(1xlen(X_cnad))
+            The utilities of all instances of X_cand(if return_utilities=True).
+        """
+
+        # check X_cand to be a non-empty 2D array containing only finite values.
         X_cand = check_array(X_cand, force_all_finite=False)
 
-        # TODO: @PM: please use functions outside this class if appropriate
+        # fit the classifier and get the probabilities
         self.clf.fit(X,y)
         probas = self.clf.predict_proba(X_cand)
+
+        # caculate the utilities
         with np.errstate(divide='ignore'):
             if self.method == 'least_confident':
                 utilities = -np.max(probas, axis=1)
             elif self.method == 'margin_sampling':
-                sort_probas = np.sort(probas)
+                sort_probas = np.sort(probas, axis=1)
                 utilities = sort_probas[:,-2] - sort_probas[:,-1]
             elif self.method == 'entropy':
                 utilities = -np.sum(probas * np.log(probas), axis=1)
 
-        # best_indices is a np.array (batch_size=1)
-        # utilities is a np.array (batch_size=1 x len(X_cand)
-        best_indices = np.ndarray(np.argmax(utilities))
+        # get the best insance and return it
+        best_indices = np.ndarray(shape=(1), buffer=np.argmax(utilities), dtype=int)
         if return_utilities:
-            return best_indices, utilities
+            return best_indices, utilities.reshape(1,len(utilities))
         else:
             return best_indices
 
