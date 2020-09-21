ea45e1fb9df40e5ff9d848ddf59f73242fda98cf
Marek Herde <marek.herde@uni-kassel.de>
2020-09-21 12:28:16 +0200

    Remove code not existing module in __init__.

--- skactiveml/classifier/__init__.py
+++ skactiveml/classifier/__init__.py
@@ -5,6 +5,5 @@ The :mod:`skactiveml.classifier` module # TODO.
 from ._pwc import PWC
 from ._cmm import CMM
 from ._wrapper import SklearnClassifier
-from ._multi_annot_classifier import MultiAnnotClassifier
 
-__all__ = ['PWC', 'CMM', 'SklearnClassifier', 'MultiAnnotClassifier']
+__all__ = ['PWC', 'CMM', 'SklearnClassifier']
