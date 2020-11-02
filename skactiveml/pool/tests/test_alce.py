88922861f83d5d999484d417525ce1bd927fe3ac
christoph14 <christoph.sandrock12@gmail.com>
2020-11-02 09:36:51 +0100

    New tests for ALCE (#3)

--- skactiveml/pool/tests/test_alce.py
+++ skactiveml/pool/tests/test_alce.py
@@ -1,8 +1,10 @@
 import numpy as np
 import unittest
 
-from skactiveml.pool import ALCE
+from sklearn.svm import SVR
+
 from skactiveml.classifier import PWC
+from skactiveml.pool import ALCE
 
 
 class TestPWC(unittest.TestCase):
@@ -10,17 +12,44 @@ class TestPWC(unittest.TestCase):
     def setUp(self):
         self.X = np.zeros((6, 2))
         self.y = [0, 1, 1, 0, 2, 1]
-        self.X_cand = np.zeros((2, 2))
+        self.X_cand = np.zeros((5, 2))
         self.classes = [0, 1, 2]
         self.C = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        self.clf = PWC(classes=self.classes)
+        self.regressor = SVR()
+        self.pwc = PWC()
 
     def test_init(self):
-        alce = ALCE(self.clf, self.C)
+        alce = ALCE(self.regressor, self.C)
         alce.query(self.X_cand, self.X, self.y)
 
+        self.assertRaises(TypeError, ALCE)
+
+        alce = ALCE(base_regressor=self.pwc, C=self.C)
+        self.assertRaises(TypeError, alce.query, self.X_cand, self.X, self.y)
+
+        alce = ALCE(base_regressor=self.regressor, C='A')
+        self.assertRaises(ValueError, alce.query, self.X_cand, self.X, self.y)
+
+        alce = ALCE(base_regressor=self.regressor, C=self.C, embed_dim=1.5)
+        self.assertRaises(TypeError, alce.query, self.X_cand, self.X, self.y)
+
+        alce = ALCE(base_regressor=self.regressor, C=self.C, embed_dim=0)
+        self.assertRaises(ValueError, alce.query, self.X_cand, self.X, self.y)
+
     def test_query(self):
-        pass
+        alce = ALCE(base_regressor=self.regressor, C=self.C)
+        alce.query(self.X_cand, self.X, self.y)
+        self.assertRaises(ValueError, alce.query, X_cand=[], X=[], y=[])
+        self.assertRaises(ValueError, alce.query, [], self.X, self.y)
+        self.assertRaises(ValueError, alce.query, self.X_cand, self.X,
+                          y=[0, 1, 4, 0, 2, 1])
+        self.assertRaises(ValueError, alce.query, X_cand=np.zeros((2, 3)),
+                          X=self.X, y=self.y)
+
+    def test_scenario(self):
+        alce = ALCE(base_regressor=self.regressor, C=1-np.eye(2))
+        query_indices = alce.query([[0], [100], [200]], [[0], [200]], [0, 1])
+        np.testing.assert_array_equal(query_indices, [1])
 
 
 if __name__ == '__main__':
