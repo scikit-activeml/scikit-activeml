ab0563117ef142839981fd8c9f2ca44cde2eb8f2
Daniel Kottke <daniel.kottke@uni-kassel.de>
2020-11-26 07:57:03 +0100

    general unittest for pool #59 corrected tests for RandomSampler

--- skactiveml/pool/tests/test_pool.py
+++ skactiveml/pool/tests/test_pool.py
@@ -1,5 +1,5 @@
-import unittest
 import inspect
+import unittest
 from importlib import import_module
 from os import path
 
@@ -28,13 +28,22 @@ class TestGeneral(unittest.TestCase):
     def test_al_cycle(self):
         for init_budget in [5, 1, 0]:
             for qs_name in self.query_strategies:
+                if qs_name == "FourDS":
+                    clf = CMM(classes=np.unique(self.y_true),
+                              missing_label=MISSING_LABEL)
+                else:
+                    clf = PWC(classes=np.unique(self.y_true),
+                              missing_label=MISSING_LABEL)
+
                 with self.subTest(msg="Basic AL Cycle",
                                   init_budget=init_budget, qs_name=qs_name):
                     y = np.full(self.y_true.shape, self.MISSING_LABEL)
                     y[0:init_budget] = self.y_true[0:init_budget]
 
-                    qs, clf = self._initialize_query_strategy(qs_name)
-
+                    qs = initialize_class_with_kwargs(
+                        self.query_strategies[qs_name], clf=clf,
+                        perf_est=clf, classes=np.unique(self.y_true),
+                        random_state=1)
 
                     for b in range(self.budget):
                         unlabeled = np.where(is_unlabeled(y))[0]
@@ -47,12 +56,13 @@ class TestGeneral(unittest.TestCase):
                         y[sample_id] = self.y_true[sample_id]
 
     def test_param(self):
+        not_test = ['self', 'kwargs']
         for qs_name in self.query_strategies:
             with self.subTest(msg="Param Test", qs_name=qs_name):
                 # get init_params
                 qs_class = self.query_strategies[qs_name]
                 init_params = list(inspect.signature(
-                                    qs_class).parameters.keys())
+                    qs_class).parameters.keys())
 
                 # check init params
                 values = [Dummy() for i in range(len(init_params))]
@@ -64,39 +74,34 @@ class TestGeneral(unittest.TestCase):
                     self.assertEqual(getattr(qs_obj, param), value)
 
                 # get query_params
-                qs, clf = self._initialize_query_strategy(qs_name)
                 query_params = list(inspect.signature(
-                    qs.query).parameters.keys())
+                    qs_class.query).parameters.keys())
 
                 # get test class to check
                 class_file_name = path.basename(inspect.getfile(qs_class))[:-3]
                 mod = import_module(
                     'skactiveml.pool.tests.test' + class_file_name)
-                self.assertTrue(hasattr(mod, 'Test' + qs_class.__name__),
-                                msg='{} has no test called {}'.format(qs_name,
-                                                                      'Test' + qs_class.__name__))
-                test_obj = getattr(mod, 'Test' + qs_class.__name__)
+                test_class_name = 'Test' + qs_class.__name__
+                self.assertTrue(hasattr(mod, test_class_name),
+                                msg='{} has no test called {}' +
+                                    ''.format(qs_name, test_class_name))
+                test_obj = getattr(mod, test_class_name)
+
+                # check init params
+                for param in np.setdiff1d(init_params, not_test):
+                    test_func_name = 'test_init_param_' + param
+                    self.assertTrue(
+                        hasattr(test_obj, test_func_name),
+                        msg="'{}()' missing for parameter '{}' of  "
+                            "__init__()".format(test_func_name, param))
 
                 # check query params
-                for param in query_params:
+                for param in np.setdiff1d(query_params, not_test):
+                    test_func_name = 'test_query_param_' + param
                     self.assertTrue(
-                        hasattr(test_obj, 'test_query_param_' + param),
-                        msg='"{}" param not tested for query()'.format(param))
-
-
-    def _initialize_query_strategy(self, qs_name):
-        if qs_name == "FourDS":
-            clf = CMM(classes=np.unique(self.y_true),
-                      missing_label=MISSING_LABEL)
-        else:
-            clf = PWC(classes=np.unique(self.y_true),
-                      missing_label=MISSING_LABEL)
-
-        qs = initialize_class_with_kwargs(
-            self.query_strategies[qs_name],
-            clf=clf, perf_est=clf, classes=np.unique(self.y_true),
-            random_state=1)
-        return qs, clf
+                        hasattr(test_obj, test_func_name),
+                        msg="'{}()' missing for parameter '{}' of "
+                            "query()".format(test_func_name, param))
 
 class Dummy:
     def __init__(self):
