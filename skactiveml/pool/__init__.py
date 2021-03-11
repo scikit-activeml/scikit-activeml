5a6db256111765c80e3ccd8e469fe79486a589aa
Daniel Kottke <daniel.kottke@uni-kassel.de>
2021-03-11 10:52:17 +0100

    update

--- skactiveml/pool/__init__.py
+++ skactiveml/pool/__init__.py
@@ -3,13 +3,14 @@ The :mod:`skactiveml.pool` module implements query strategies for pool-based
 active learning.
 """
 
-from ._probal import McPAL
+from ._probal import McPAL, XPAL
 from ._random import RandomSampler
 from ._uncertainty import UncertaintySampling, expected_average_precision
 from ._epistemic_uncertainty import EpistemicUncertainty
 from ._qbc import QBC, average_kl_divergence, vote_entropy
 from ._expected_error import ExpectedErrorReduction
 from ._four_ds import FourDS
+from ._alce import ALCE
 from ._optimal import Optimal
 
 __all__ = ['RandomSampler', 'McPAL', 'UncertaintySampling',
