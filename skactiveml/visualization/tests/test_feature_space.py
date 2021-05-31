d7d95cfa9a1dba0f6aba99c7793bb6580ab55c12
Atal Roghman <atalroghman@gmail.com>
2021-05-31 04:02:36 +0200

    Input data validation tests

--- skactiveml/visualization/tests/test_feature_space.py
+++ skactiveml/visualization/tests/test_feature_space.py
@@ -19,31 +19,31 @@ from sklearn.linear_model import LogisticRegression
 class TestFeatureSpace(unittest.TestCase):
 
     def setUp(self):
-        pass
-        #self.X = make_classification(n_features=2, n_redundant=0, random_state=0)[0]
-        #self.y_oracle = make_classification(n_features=2, n_redundant=0, random_state=0)[1]
-        #self.y = np.full(shape=self.y_oracle.shape, fill_value=[0, 1])
+
+        self.X = make_classification(n_features=2, n_redundant=0, random_state=0)[0]
+        self.y_oracle = make_classification(n_features=2, n_redundant=0, random_state=0)[1]
+        self.y = np.full(shape=self.y_oracle.shape, fill_value=[0, 1])
 
 
     def test_input_validation(self):
-        pass
-        #self.assertEqual(len(self.X), len(self.y))
-        #self.assertEqual(len(self.y), len(self.y_oracle))
+
+        self.assertEqual(len(self.X), len(self.y))
+        self.assertEqual(len(self.y), len(self.y_oracle))
 
 
     def test_on_data_set(self):
-        pass
-        #X, y_true = make_classification(n_features=2, n_redundant=0, random_state=0)
-        #clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-        #qs = UncertaintySampling(clf, method='entropy', random_state=42)
-        #y = np.full(shape=y_true.shape, fill_value=MISSING_LABEL)
-        #unlbld_idx = np.where(is_unlabeled(y))[0]
-        #X_cand = X[unlbld_idx]
-        #query_idx = unlbld_idx[qs.query(X_cand=X_cand, X=X, y=y, batch_size=1)]
-        #y[query_idx] = y_true[query_idx]
-        #clf.fit(X, y)
-        #y_pred = clf.predict(X)
-        #self.assertTrue(accuracy_score(y_true, y_pred) > 0.5)
+
+        X, y_true = make_classification(n_features=2, n_redundant=0, random_state=0)
+        clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
+        qs = UncertaintySampling(clf, method='entropy', random_state=42)
+        y = np.full(shape=y_true.shape, fill_value=MISSING_LABEL)
+        unlbld_idx = np.where(is_unlabeled(y))[0]
+        X_cand = X[unlbld_idx]
+        query_idx = unlbld_idx[qs.query(X_cand=X_cand, X=X, y=y, batch_size=1)]
+        y[query_idx] = y_true[query_idx]
+        clf.fit(X, y)
+        y_pred = clf.predict(X)
+        self.assertTrue(accuracy_score(y_true, y_pred) > 0.5)
 
 
 
