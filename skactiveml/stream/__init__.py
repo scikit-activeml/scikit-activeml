267f3e1b300d932f9272845335616ef2afe6b5de
AlexanderBenz <alexbenz7@web.de>
2021-07-01 12:25:54 +0200

    move delay_wrapper to subfolder

--- skactiveml/stream/__init__.py
+++ skactiveml/stream/__init__.py
@@ -1,16 +1,11 @@
 """
-The :mod:`skactiveml.pool` module implements query strategies for stream-based
-active learning.
+The :mod:`skactiveml.stream` module implements query strategies for
+stream-based active learning.
 """
 
 from ._random import RandomSampler, PeriodicSampler
 from ._pal import PAL
 from ._uncertainty import FixedUncertainty, VariableUncertainty, Split
-from ._delay_wrapper import (
-    BaggingDelaySimulationWrapper,
-    ForgettingWrapper,
-    FuzzyDelaySimulationWrapper,
-)
 
 __all__ = [
     "RandomSampler",
@@ -19,7 +14,4 @@ __all__ = [
     "VariableUncertainty",
     "Split",
     "PAL",
-    "BaggingDelaySimulationWrapper",
-    "ForgettingWrapper",
-    "FuzzyDelaySimulationWrapper",
 ]
