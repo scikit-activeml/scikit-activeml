78cc25b0e0be527a7a917f11ee3a6efa450bbc22
Daniel Kottke <daniel.kottke@uni-kassel.de>
2022-02-02 19:12:22 +0100

    cleanup eer

--- skactiveml/pool/_expected_error.py
+++ skactiveml/pool/_expected_error.py
@@ -11,15 +11,11 @@ from ..utils import check_type, is_labeled, simple_batch, \
     fit_if_not_fitted, check_cost_matrix, check_X_y, MISSING_LABEL, \
     check_equal_missing_label, labeled_indices, unlabeled_indices, is_unlabeled
 
-
-class ExpectedErrorReduction_old(SingleAnnotPoolBasedQueryStrategy):
+class ExpectedErrorReduction(SingleAnnotPoolBasedQueryStrategy):
     """Expected Error Reduction
 
-    This class implements the expected error reduction algorithm with different
-    loss functions:
-     - log loss (log_loss) [1],
-     - expected misclassification risk (emr) [2],
-     - and cost-sensitive learning (csl) [3].
+    TODO: This class implements the expected error reduction algorithm with
+    different loss functions:
 
     Parameters
     ----------
@@ -49,23 +45,22 @@ class ExpectedErrorReduction_old(SingleAnnotPoolBasedQueryStrategy):
         In IJCAI (Vol. 5, pp. 1622-1623).
     """
 
-    LOG_LOSS = 'log_loss' # [1] eval on X (ind. of X_cand)
-    ZERO_ONE = 'zero_one' # [1] eval on X (ind. of X_cand)
-    EMR = 'emr' # [2] eval on X without x_cand (mapping req.), vgl. Kapoor
-                    # (diff: exclude_labeled_samples)
-    CSL = 'csl' # [3] eval on labeled in X,y (ind. of X_cand)
-
-    def __init__(self, method=EMR, cost_matrix=None, ignore_partial_fit=False,
-                 random_state=None):
-        super().__init__(random_state=random_state)
-        self.method = method
+    def __init__(self, enforce_mapping, cost_matrix=None,
+                 missing_label=MISSING_LABEL, random_state=None):
+        super().__init__(
+            missing_label=missing_label, random_state=random_state
+        )
         self.cost_matrix = cost_matrix
-        self.ignore_partial_fit = ignore_partial_fit
+        self._enforce_mapping = enforce_mapping
 
-    def query(self, X, y, clf, fit_clf=True, sample_weight=None,
-              candidates=None, batch_size=1, return_utilities=False):
+    def query(self, X, y, clf, sample_weight=None,
+              fit_clf=True, ignore_partial_fit=True,
+              candidates=None, sample_weight_candidates=None,
+              X_eval=None, sample_weight_eval=None,
+              batch_size=1, return_utilities=False):
         """Determines for which candidate samples labels are to be queried.
 
+        TODO
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
@@ -117,42 +112,6 @@ class ExpectedErrorReduction_old(SingleAnnotPoolBasedQueryStrategy):
             If candidates is of shape (n_candidates, n_features), the indexing
             refers to samples in candidates.
         """
-        # Validate input parameters.
-        X, y, candidates, batch_size, return_utilities = self._validate_data(
-                X, y, candidates, batch_size, return_utilities, reset=True
-            )
-
-        X_cand, mapping = self._transform_candidates(candidates, X, y)
-        sample_weight_cand = sample_weight[mapping]
-
-        # Calculate utilities
-        utilities = expected_error_reduction(
-            clf=clf, X_cand=X_cand, X=X, y=y, cost_matrix=self.cost_matrix,
-            method=self.method, sample_weight=sample_weight,
-            sample_weight_cand=sample_weight_cand,
-            ignore_partial_fit=self.ignore_partial_fit
-        )
-
-        return simple_batch(utilities, self.random_state_,
-                            batch_size=batch_size,
-                            return_utilities=return_utilities)
-
-class ExpectedErrorReduction(SingleAnnotPoolBasedQueryStrategy):
-
-    def __init__(self, enforce_mapping, cost_matrix=None,
-                 missing_label=MISSING_LABEL, random_state=None):
-        super().__init__(
-            missing_label=missing_label, random_state=random_state
-        )
-        self.cost_matrix = cost_matrix
-        self._enforce_mapping = enforce_mapping
-
-    def query(self, X, y, clf, sample_weight=None,
-              fit_clf=True, ignore_partial_fit=True,
-              candidates=None, sample_weight_candidates=None,
-              X_eval=None, sample_weight_eval=None,
-              batch_size=1, return_utilities=False):
-
         (
             X, y, sample_weight, clf, candidates, sample_weight_candidates,
             X_eval, sample_weight_eval, batch_size, return_utilities
@@ -486,9 +445,6 @@ class ValueOfInformationEER(ExpectedErrorReduction):
         return uclf
 
 
-
-
-
 class UpdateIndexClassifier():
     def __init__(self, clf, X, y, sample_weight,
                  fit_clf=True, ignore_partial_fit=True):
@@ -598,158 +554,3 @@ class UpdateIndexClassifier():
             return res
         else:
             return self.clf_.predict_proba(self.X[idx])
-
-
-def expected_error_reduction(clf, X_cand, X=None, y=None, cost_matrix=None,
-                             method='emr', sample_weight_cand=None,
-                             sample_weight=None, ignore_partial_fit=False):
-    """Compute uncertainty scores.
-
-    In case of a given cost matrix C, maximum expected cost is implemented as
-    score.
-
-    Parameters
-    ----------
-    clf : skactiveml.base.SkactivemlClassifier
-        Model implementing the methods `fit` and `predict_proba`.
-    X_cand : array-like, shape (n_candidate_samples, n_features)
-        Candidate samples from which the strategy can select.
-    X : array-like, shape (n_samples, n_features), optional (default=None)
-        Complete training data set.
-    y : array-like, shape (n_samples), optional (default=None)
-        Labels of the training data set.
-    cost_matrix : array-like, shape (n_classes, n_classes), optional
-    (default=None)
-        Cost matrix with `cost_matrix[i,j]` defining the cost of predicting
-        class `j` for a sample with the actual class `i`.
-        Only supported for least confident
-        variant.
-    method : {'log_loss', 'emr', 'csl'}, optional (default='emr')
-        Variant of expected error reduction to be used: 'log_loss' is
-        cost-insensitive, while 'emr' and 'csl' are cost-sensitive variants.
-    sample_weight : array-like, shape (n_samples), optional
-    (default=None)
-        Weights of training samples in `X`.
-    sample_weight_cand : array-like, shape (n_candidate_samples), optional
-    (default=None)
-        Weights of candidate samples in `X_cand`.
-    ignore_partial_fit : bool, optional (default=False)
-        If false, the classifier will be refitted through `partial_fit` if
-        available. Otherwise, the use of `fit` is enforced.
-
-    Returns
-    -------
-    utilities : np.ndarray, shape (n_candidates)
-        The utilities of all unlabeled instances.
-
-    References
-    ----------
-    [1] Settles, Burr. "Active learning literature survey." University of
-        Wisconsin, Madison 52.55-66 (2010): 11.
-    [2] Joshi, A. J., Porikli, F., & Papanikolopoulos, N. (2009, June).
-        Multi-class active learning for image classification.
-        In 2009 IEEE Conference on Computer Vision and Pattern Recognition
-        (pp. 2372-2379). IEEE.
-    [3] Margineantu, D. D. (2005, July). Active cost-sensitive learning.
-        In IJCAI (Vol. 5, pp. 1622-1623).
-    """
-    # Check if the classifier and its arguments are valid.
-    check_type(clf, 'clf', SkactivemlClassifier)
-
-    # Check whether to use `fit` or `partial_fit`.
-    check_type(ignore_partial_fit, 'ignore_partial_fit', bool)
-    use_fit = ignore_partial_fit or not hasattr(clf, 'partial_fit')
-    if use_fit and (X is None or y is None):
-        raise ValueError(
-            '`X` and `y` cannot be None for a classifier using `fit` for '
-            'retraining.'
-        )
-    if (X is None or y is None) and method == 'csl':
-        raise ValueError(
-            "`X` and `y` cannot be None for `method='csl'`."
-        )
-    use_sample_weight = sample_weight is not None \
-                        or sample_weight_cand is not None
-    if use_fit and (bool(sample_weight is None)
-                    != bool(sample_weight_cand is None)):
-        raise ValueError(
-            '`sample_weight` and `sample_weight_cand` must either both be '
-            'None or array-like, if the fit method is used.'
-        )
-    X, y, X_cand, sample_weight, sample_weight_cand = check_X_y(
-        X, y, X_cand, sample_weight, sample_weight_cand,
-        force_all_finite=False, missing_label=clf.missing_label,
-        allow_nd=True
-    )
-
-    # Refit classifier.
-    if use_sample_weight:
-        clf = fit_if_not_fitted(clf, X, y, sample_weight, False)
-    else:
-        clf = fit_if_not_fitted(clf, X, y, None, False)
-    clf_refit = clone(clf).fit if use_fit else deepcopy(clf).partial_fit
-
-    # Check cost matrix.
-    n_classes = len(clf.classes_)
-    cost_matrix = 1 - np.eye(len(clf.classes_)) if cost_matrix is None else \
-        cost_matrix
-    cost_matrix = check_cost_matrix(cost_matrix, n_classes)
-
-    # Compute class-membership probabilities of candidate samples.
-    P = clf.predict_proba(X_cand)
-
-    # Storage for computed errors per candidate sample.
-    errors = np.zeros(len(X_cand))
-    errors_per_class = np.zeros(n_classes)
-
-    # Iterate over candidate samples
-    for i, x in enumerate(X_cand):
-        # Simulate acquisition of label for each candidate sample and class.
-        for yi in range(n_classes):
-            # Create sample array for the retraining of the classifier.
-            X_new = np.vstack((X, [x])) if use_fit else np.array([x])
-            # Create label array for the retraining of the classifier.
-            y_new = np.append(y, [[yi]]) if use_fit else np.array([yi])
-            # Check whether sample weights are used.
-            if use_sample_weight:
-                # Create sample weight array for the retraining of the
-                # classifier.
-                w = sample_weight_cand[i]
-                if use_fit:
-                    sample_weight_new = np.append(sample_weight, [[w]])
-                else:
-                    sample_weight_new = np.array([w])
-                # Retrain classifier with sample weights.
-                clf_new = clf_refit(X_new, y_new, sample_weight_new)
-            else:
-                # Retrain classifier without sample weights.
-                clf_new = clf_refit(X_new, y_new)
-            if method == 'emr':
-                P_new = clf_new.predict_proba(X_cand)
-                costs = np.sum((P_new.T[:, None] * P_new.T).T * cost_matrix)
-            elif method == 'csl':
-                is_lbld = is_labeled(y, clf_new.missing_label)
-                X_labeled = X[is_lbld]
-                y_labeled = y[is_lbld]
-                y_indices = [np.where(clf_new.classes_ == label)[0][0]
-                             for label in y_labeled]
-                if len(X_labeled) > 0:
-                    costs = np.sum(
-                        clf_new.predict_proba(X_labeled) *
-                        cost_matrix[y_indices]
-                    )
-                else:
-                    costs = 0
-            elif method == 'log_loss':
-                P_new = clf_new.predict_proba(X_cand)
-                costs = -np.sum(P_new * np.log(P_new + np.finfo(float).eps))
-            else:
-                raise ValueError(
-                    f"Supported methods are [{ExpectedErrorReduction.EMR}, "
-                    f"{ExpectedErrorReduction.CSL}, "
-                    f"{ExpectedErrorReduction.LOG_LOSS}], the given one is: "
-                    f"{method}"
-                )
-            errors_per_class[yi] = P[i, yi] * costs
-        errors[i] = errors_per_class.sum()
-    return -errors
