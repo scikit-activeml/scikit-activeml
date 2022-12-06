f68aa2270510b97436aa2d1487ee5552e512b01e
tpham93 <tuanphamminh93@gmail.com>
2022-12-06 17:41:31 +0100

    Add **kwargs handling for call_func

--- skactiveml/utils/tests/test_functions.py
+++ skactiveml/utils/tests/test_functions.py
@@ -20,6 +20,37 @@ class TestFunctions(unittest.TestCase):
         result = call_func(dummy_function, only_mandatory=True, a=2, b=5, c=5)
         self.assertEqual(result, 12)
 
+        # test kwargs
+        def sum_all(*args, **kwargs):
+            s = 0
+            for v in args + tuple(kwargs.values()):
+                s += v
+            return s
+
+        def test_func_0(**kwargs):
+            return sum_all(**kwargs)
+
+        def test_func_1(arg1, **kwargs):
+            return sum_all(arg1, **kwargs)
+
+        def test_func_2(kwarg1=0, **kwargs):
+            return sum_all(kwarg1=kwarg1, **kwargs)
+
+        result = call_func(test_func_0, arg1=1, arg2=2, arg3=3)
+        self.assertEqual(result, 6)
+
+        result = call_func(test_func_1, arg1=1, arg2=2, arg3=3)
+        self.assertEqual(result, 6)
+
+        result = call_func(test_func_2, kwarg1=1, arg2=2, arg3=3)
+        self.assertEqual(result, 6)
+
+        result = call_func(test_func_1, only_mandatory=True, arg1=1, arg2=2, arg3=3)
+        self.assertEqual(result, 1)
+
+        result = call_func(test_func_2, only_mandatory=True, kwarg1=1, arg2=2, arg3=3)
+        self.assertEqual(result, 0)
+
     def test__available_if(self):
 
         if hasattr(metaestimators, "available_if"):
