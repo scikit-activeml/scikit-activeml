b685c7be276c7e9877dc5a4805ee97785ab9b41c
Cheng-JY <chengjiaying.prc@gmail.com>
2023-11-14 15:58:06 +0100

    feat: add core set and a corresponding notebook

--- skactiveml/pool/_core_set.py
+++ skactiveml/pool/_core_set.py
@@ -6,3 +6,25 @@ import numpy as np
 
 from ..base import SingleAnnotatorPoolQueryStrategy
 from ..utils import MISSING_LABEL, simple_batch
+
+class CoreSet(SingleAnnotatorPoolQueryStrategy):
+    
+    def __init__(
+          self,
+          missing_label=MISSING_LABEL,
+          random_state=None  
+    ):
+        super().__init__(
+            missing_label=missing_label, random_state=random_state
+        )
+
+    def query(
+        self, X, y, candidates=None, batch_size=1, return_utilities=False
+    ):
+        X, y, candidates, batch_size, return_utilities = self._validate_data(
+            X, y, candidates, batch_size, return_utilities, reset=True
+        )
+
+        return X
+    
+    
