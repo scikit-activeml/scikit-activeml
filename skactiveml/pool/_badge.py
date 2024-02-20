1642761701bc99b185a7e4d48973f066f8eb8ab9
Cheng-JY <chengjiaying.prc@gmail.com>
2024-02-20 14:32:14 +0100

    fix: pack clf_embedding_flag in init

--- skactiveml/pool/_badge.py
+++ skactiveml/pool/_badge.py
@@ -38,21 +38,26 @@ class Badge(SingleAnnotatorPoolQueryStrategy):
          by Diverse, Uncertain Gradient Lower Bounds." ICLR, 2019.
     """
 
-    def __init__(self, missing_label=MISSING_LABEL, random_state=None):
+    def __init__(
+        self,
+        clf_embedding_flag=None,
+        missing_label=MISSING_LABEL,
+        random_state=None,
+    ):
+        self.clf_embedding_flag = clf_embedding_flag
         super().__init__(
             missing_label=missing_label, random_state=random_state
         )
 
     def query(
-            self,
-            X,
-            y,
-            clf,
-            fit_clf=True,
-            candidates=None,
-            batch_size=1,
-            return_utilities=False,
-            clf_embedding_flag=None,
+        self,
+        X,
+        y,
+        clf,
+        fit_clf=True,
+        candidates=None,
+        batch_size=1,
+        return_utilities=False,
     ):
         """Query the next samples to be labeled
 
@@ -123,8 +128,8 @@ class Badge(SingleAnnotatorPoolQueryStrategy):
         check_type(clf, "clf", SkactivemlClassifier)
         check_equal_missing_label(clf.missing_label, self.missing_label_)
         check_scalar(fit_clf, "fit_clf", bool)
-        if clf_embedding_flag is not None:
-            check_scalar(clf_embedding_flag, "clf_embedding_flag", str)
+        if self.clf_embedding_flag is not None:
+            check_scalar(self.clf_embedding_flag, "clf_embedding_flag", str)
 
         # Fit the classifier
         if fit_clf:
@@ -145,9 +150,9 @@ class Badge(SingleAnnotatorPoolQueryStrategy):
             unlbld_mapping = np.arange(len(X_cand))
 
         # gradient embedding, aka predict class membership probabilities
-        if clf_embedding_flag is not None:
+        if self.clf_embedding_flag is not None:
             probas, X_unlbld = clf.predict_proba(
-                X_unlbld, **{clf_embedding_flag: True}
+                X_unlbld, **{self.clf_embedding_flag: True}
             )
         else:
             probas = clf.predict_proba(X_unlbld)
@@ -231,7 +236,7 @@ def _d_2(g_x, query_indicies, d_latest=None):
         The D^2 value, for the first sample, is the value inf.
     """
     if len(query_indicies) == 0:
-        return np.sum(g_x ** 2, axis=-1)
+        return np.sum(g_x**2, axis=-1)
     g_query_indicies = g_x[query_indicies]
     _, D = pairwise_distances_argmin_min(X=g_x, Y=g_query_indicies)
     if d_latest is not None:
