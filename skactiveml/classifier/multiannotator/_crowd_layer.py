e1ef52893db0da2310da02668d5e9458aac5c70c
Cheng-JY <chengjiaying.prc@gmail.com>
2024-05-25 14:18:04 +0200

    feat: add some documentation

--- skactiveml/classifier/multiannotator/_crowd_layer.py
+++ skactiveml/classifier/multiannotator/_crowd_layer.py
@@ -12,6 +12,47 @@ from ...utils import ExtLabelEncoder
 
 
 class CrowdLayerClassifier(SkorchClassifier, AnnotatorModelMixin):
+    """
+    CrowdLayerClassifier
+
+    CrowdLayer [1] is a layer added at the end of a classifying neural network
+    and allows us to train deep neural networks end-to-end, directly from the
+    noisy labels of multiple annotators, using only backpropagation.
+
+    Parameters
+    ----------
+    module__n_annotators : int
+         Number of annotators.
+    module__gt_net : nn.Module
+        Pytorch module of the GT model taking samples
+        as input to predict class-membership logits.
+    arguments
+        more possible arguments for initialize your neural network
+        see: https://skorch.readthedocs.io/en/stable/net.html
+    classes : array-like of shape (n_classes,), default=None
+        Holds the label for each class. If none, the classes are determined
+        during the fit.
+    missing_label : scalar or string or np.nan or None, default=np.nan
+        Value to represent a missing label.
+    cost_matrix : array-like of shape (n_classes, n_classes)
+        Cost matrix with `cost_matrix[i,j]` indicating cost of predicting class
+        `classes[j]` for a sample of class `classes[i]`. Can be only set, if
+        `classes` is not none.
+    random_state : int or RandomState instance or None, default=None
+        Determines random number for 'predict' method. Pass an int for
+        reproducible results across multiple method calls.
+    **kwargs : keyword arguments
+        more possible parameters to customizing your neural network
+        see: https://skorch.readthedocs.io/en/stable/net.html
+    ATTENTION: Criterion is in this methode predefined. Please don't overwrite the
+    'criterion' parameter.
+
+    References
+    ----------
+    [1] Rodrigues, Filipe, and Francisco Pereira. "Deep learning from crowds." In Proceedings of the AAAI conference on
+        artificial intelligence, vol. 32, no. 1. 2018.
+
+    """
     def __init__(self, *args, **kwargs):
         super(CrowdLayerClassifier, self).__init__(
             module=CrowdLayerModule,
@@ -29,26 +70,6 @@ class CrowdLayerClassifier(SkorchClassifier, AnnotatorModelMixin):
         return loss
 
     def fit(self, X, y, **fit_params):
-        """Fit the model using X as training data and y as annotation from annotators.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            The sample matrix X is the feature matrix representing the samples.
-        y : array-like of shape (n_samples, n_estimators)
-            It contains the class labels of the training samples.
-            The number of class labels may be variable for the samples, where
-            missing labels are represented the attribute `missing_label`.
-        sample_weight : array-like of shape (n_samples, n_estimators)
-            It contains the weights of the training samples' class labels.
-            It must have the same shape as `y`.
-
-        Returns
-        -------
-        self: skactiveml.classifier.multiannotator.AnnotatorEnsembleClassifier,
-            The `AnnotatorEnsembleClassifier` object fitted on the training
-            data.
-        """
 
         self.check_X_dict_ = {
             "ensure_min_samples": 0,
@@ -56,7 +77,7 @@ class CrowdLayerClassifier(SkorchClassifier, AnnotatorModelMixin):
             "allow_nd": True,
             "dtype": None,
         }
-        X, y, sample_weight = self._validate_data(
+        X, y, _ = self._validate_data(
             X=X,
             y=y,
             check_X_dict=self.check_X_dict_,
@@ -73,24 +94,21 @@ class CrowdLayerClassifier(SkorchClassifier, AnnotatorModelMixin):
         P_class = P_class.numpy()
         P_annot = F.softmax(logits_annot, dim=1)
         P_annot = P_annot.numpy()
-        P_perf = np.array(
-            [
-                np.einsum("ij,ik->ijk", P_class, P_annot[:, :, i])
-                for i in range(n_annotators)
-            ]
-        )
+        P_perf = np.array([np.einsum("ij,ik->ijk", P_class, P_annot[:, :, i]) for i in range(n_annotators)])
         P_perf = P_perf.swapaxes(0, 1)
         if return_confusion_matrix:
             return P_perf
         return P_perf.diagonal(axis1=-2, axis2=-1).sum(axis=-1)
 
     def predict(self, X):
-        p_class = self.predict_proba(X)
+        # maybe flag to switch between mode
+        p_class, logits_annot = self.forward(X)
         return p_class.argmax(axis=1)
 
     def predict_proba(self, X):
-        p_class = self.forward(X, return_logits_annotators=False)
-        return p_class.numpy()
+        P_class, logits_annot = self.forward(X)
+        P_class = P_class.numpy()
+        return P_class
 
     def validation_step(self, batch, **fit_params):
         # not for loss but for acc
@@ -99,17 +117,45 @@ class CrowdLayerClassifier(SkorchClassifier, AnnotatorModelMixin):
             y_pred = self.predict(Xi)
             acc = torch.mean((y_pred == yi).float())
         return {
-            "acc": acc,
-            "y_pred": y_pred,
+            'loss': acc,
+            'y_pred': y_pred,
         }
+
+    def predict_P_annot(self, X):
+        _, logits_annot = self.forward(X)
+        P_annot = F.softmax(logits_annot, dim=1)
+        P_annot = P_annot.numpy()
+        return P_annot
 
 
 class CrowdLayerModule(nn.Module):
+    """
+    CrowdLayerModule
+
+    CrowdLayer [1] is a layer added at the end of a classifying neural network
+    and allows us to train deep neural networks end-to-end, directly from the
+    noisy labels of multiple annotators, using only backpropagation.
+
+    Parameters
+    ----------
+    n_classes : int
+        Number of classes.
+    n_annotators : int
+        Number of annotators.
+    gt_net : nn.Module
+        Pytorch module of the GT model taking samples
+        as input to predict class-membership logits.
+
+    References
+    ----------
+    [1] Rodrigues, Filipe, and Francisco Pereira. "Deep learning from crowds." In Proceedings
+    of the AAAI conference on artificial intelligence, vol. 32, no. 1. 2018.
+    """
     def __init__(
-        self,
-        n_classes,
-        n_annotators,
-        gt_net,
+            self,
+            n_classes,
+            n_annotators,
+            gt_net,
     ):
         super().__init__()
         self.n_classes = n_classes
@@ -120,18 +166,16 @@ class CrowdLayerModule(nn.Module):
         self.annotator_layers = nn.ModuleList()
         for i in range(n_annotators):
             layer = nn.Linear(n_classes, n_classes, bias=False)
-            layer.weight = nn.Parameter(torch.eye(n_classes) * 10)
+            layer.weight = nn.Parameter(torch.eye(n_classes))
             self.annotator_layers.append(layer)
 
-    def forward(self, x, return_logits_annotators=True):
+    def forward(self, x):
         """Forward propagation of samples through the GT and AP (optional) model.
 
         Parameters
         ----------
         x : torch.Tensor of shape (batch_size, *)
             Samples.
-        return_logits_annot: bool, optional (default=True)
-            Flag whether the annotation logits are to be returned, next to the class-membership probabilities.
 
         Returns
         -------
@@ -146,9 +190,6 @@ class CrowdLayerModule(nn.Module):
         # Compute class-membership probabilities.
         p_class = F.softmax(logit_class, dim=-1)
 
-        if not return_logits_annotators:
-            return p_class
-
         # Compute logits per annotator.
         logits_annot = []
         for layer in self.annotator_layers:
@@ -156,3 +197,4 @@ class CrowdLayerModule(nn.Module):
         logits_annot = torch.stack(logits_annot, dim=2)
 
         return p_class, logits_annot
+
