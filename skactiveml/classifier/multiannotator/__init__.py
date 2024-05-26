8023c812e52bef9663085cf5708af65f9acf8188
Cheng-JY <chengjiaying.prc@gmail.com>
2024-05-26 13:28:06 +0200

    rename file

--- skactiveml/classifier/multiannotator/__init__.py
+++ skactiveml/classifier/multiannotator/__init__.py
@@ -1,5 +1,5 @@
 from ._annotator_ensemble_classifier import AnnotatorEnsembleClassifier
 from ._annotator_logistic_regression import AnnotatorLogisticRegression
-from ._crowd_layer import CrowdLayerClassifier
+from ._crowd_layer_classifier import CrowdLayerClassifier
 
 __all__ = ["AnnotatorLogisticRegression", "AnnotatorEnsembleClassifier", "CrowdLayerClassifier"]
