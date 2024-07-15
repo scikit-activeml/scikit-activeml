feb9ba31e577fc558b4646a45d09f1e1ccc8485e
Cheng-JY <chengjiaying.prc@gmail.com>
2024-07-15 18:01:06 +0200

    add new classifier

--- skactiveml/classifier/multiannotator/__init__.py
+++ skactiveml/classifier/multiannotator/__init__.py
@@ -1,9 +1,11 @@
 from ._annotator_ensemble_classifier import AnnotatorEnsembleClassifier
 from ._annotator_logistic_regression import AnnotatorLogisticRegression
 from ._crowd_layer_classifier import CrowdLayerClassifier
+from ._reg_crowd_net_classifier import RegCrowdNetClassifier
 
 __all__ = [
     "AnnotatorLogisticRegression",
     "AnnotatorEnsembleClassifier",
     "CrowdLayerClassifier",
+    "RegCrowdNetClassifier",
 ]
