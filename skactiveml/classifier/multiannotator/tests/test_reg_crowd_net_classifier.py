e13289754e5a62f53bc52e4f1ec855539cec2e48
Cheng-JY <chengjiaying.prc@gmail.com>
2024-07-16 13:17:50 +0200

    add some test

--- skactiveml/classifier/multiannotator/tests/test_reg_crowd_net_classifier.py
+++ skactiveml/classifier/multiannotator/tests/test_reg_crowd_net_classifier.py
@@ -11,16 +11,17 @@ from skorch.dataset import Dataset
 from skactiveml.classifier.multiannotator import RegCrowdNetClassifier
 
 
-class TestCrowdLayerClassifier(unittest.TestCase):
+class TestRegCrowdNetClassifierClassifier(unittest.TestCase):
     def setUp(self):
         self.X, self.y_true = make_blobs(n_samples=300, random_state=0)
         self.X = self.X.astype(np.float32)
         self.y = np.array([self.y_true, self.y_true], dtype=float).T
         self.y[:100, 0] = -1
-        self.clf_init_params = {
+        self.default_params = {
             "n_classes": 3,
-            ""
-            "module__n_annotators": 2,
+            "n_annotators": 2,
+        }
+        self.clf_init_params = {
             "classes": [0, 1, 2],
             "missing_label": -1,
             "cost_matrix": None,
@@ -29,33 +30,45 @@ class TestCrowdLayerClassifier(unittest.TestCase):
             "verbose": False,
             "optimizer": torch.optim.RAdam,
             "device": "cpu",
-            "max_epochs": 10,
+            "max_epochs": 5,
             "batch_size": 1,
             "lr": 0.001,
         }
 
-    def test_init_param_module_gt_net(self):
-        clf = CrowdLayerClassifier(module__gt_net="Test")
-        self.assertEqual(clf.module__gt_net, "Test")
+    def test_init_param_module_gt_embed_x(self):
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x="Test",
+            **self.default_params,
+        )
+        self.assertEqual(clf.module__gt_embed_x, "Test")
         self.assertRaises(TypeError, clf.fit, X=self.X, y=self.y)
 
-        clf = CrowdLayerClassifier(module__gt_net=None)
-        self.assertRaises(TypeError, clf.fit, X=self.X, y=self.y)
-
-        clf = CrowdLayerClassifier(
-            module__gt_net=[("nn.Module", TestNeuralNet)]
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x=None,
+            **self.default_params,
         )
         self.assertRaises(TypeError, clf.fit, X=self.X, y=self.y)
 
-        clf = CrowdLayerClassifier(
-            classes=[0, 1, 2], module__gt_net=TestNeuralNet
+        clf = RegCrowdNetClassifier(
+            module__gt_net=[("nn.Module", GT_Embed_Net)],
+            **self.default_params,
+        )
+        self.assertRaises(TypeError, clf.fit, X=self.X, y=self.y)
+
+        clf = RegCrowdNetClassifier(
+            classes=[0, 1, 2],
+            module__gt_net=GT_Embed_Net,
+            **self.default_params,
         )
         self.assertRaises(ValueError, clf.fit, X=self.X, y=self.y)
 
     def test_fit(self):
-        gt_net = TestNeuralNet()
-        clf = CrowdLayerClassifier(
-            module__gt_net=gt_net,
+        gt_embed = GT_Embed_Net()
+        gt_output = GT_Output_Net()
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x=gt_embed,
+            module__gt_output=gt_output,
+            **self.default_params,
             **self.clf_init_params,
         )
 
@@ -65,9 +78,12 @@ class TestCrowdLayerClassifier(unittest.TestCase):
         self.assertIsNone(clf.check_is_fitted())
 
     def test_predict(self):
-        gt_net = TestNeuralNet()
-        clf = CrowdLayerClassifier(
-            module__gt_net=gt_net,
+        gt_embed = GT_Embed_Net()
+        gt_output = GT_Output_Net()
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x=gt_embed,
+            module__gt_output=gt_output,
+            **self.default_params,
             **self.clf_init_params,
         )
         self.assertRaises(NotFittedError, clf.predict, X=self.X)
@@ -75,27 +91,13 @@ class TestCrowdLayerClassifier(unittest.TestCase):
         y_pred = clf.predict(self.X)
         self.assertEqual(len(y_pred), len(self.X))
 
-    def test_predict_annotator_pref(self):
-        gt_net = TestNeuralNet()
-        clf = CrowdLayerClassifier(
-            module__gt_net=gt_net,
-            **self.clf_init_params,
-        )
-        self.assertRaises(NotFittedError, clf.predict, X=self.X)
-        clf.fit(self.X, self.y)
-        annot_pref = clf.predict_annotator_perf(self.X[:2])
-        self.assertEqual(annot_pref.shape[0], 2)
-        self.assertEqual(annot_pref.shape[1], 2)
-        confusion_matrix = clf.predict_annotator_perf(self.X[:2], True)
-        self.assertEqual(confusion_matrix.shape[0], 2)
-        self.assertEqual(confusion_matrix.shape[1], 2)
-        self.assertEqual(confusion_matrix.shape[2], 3)
-        self.assertEqual(confusion_matrix.shape[3], 3)
-
     def test_predict_prob(self):
-        gt_net = TestNeuralNet()
-        clf = CrowdLayerClassifier(
-            module__gt_net=gt_net,
+        gt_embed = GT_Embed_Net()
+        gt_output = GT_Output_Net()
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x=gt_embed,
+            module__gt_output=gt_output,
+            **self.default_params,
             **self.clf_init_params,
         )
         self.assertRaises(NotFittedError, clf.predict, X=self.X)
@@ -103,6 +105,25 @@ class TestCrowdLayerClassifier(unittest.TestCase):
         proba = clf.predict_proba(self.X[:1])
         self.assertEqual(1, proba.shape[0])
         self.assertEqual(3, proba.shape[1])
+
+    def test_predict_annotator_pref(self):
+        gt_embed = GT_Embed_Net()
+        gt_output = GT_Output_Net()
+        clf = RegCrowdNetClassifier(
+            module__gt_embed_x=gt_embed,
+            module__gt_output=gt_output,
+            **self.default_params,
+            **self.clf_init_params,
+        )
+        self.assertRaises(NotFittedError, clf.predict, X=self.X)
+        clf.fit(self.X, self.y)
+        annot_pref = clf.predict_annotator_perf()
+        self.assertEqual(annot_pref.shape[0], 2)
+        self.assertEqual(annot_pref.shape[1], 2)
+        confusion_matrix = clf.predict_annotator_perf(return_confusion_matrix=True)
+        print(confusion_matrix.shape)
+        self.assertEqual(2, confusion_matrix.shape[0])
+        self.assertEqual(2, confusion_matrix.shape[1])
 
     def test_predict_P_annot(self):
         gt_net = TestNeuralNet()
