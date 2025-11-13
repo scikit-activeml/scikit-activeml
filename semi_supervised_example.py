c7681c8d8c010339e96072031945b66992df17a4
lluehrs <uk073949@student.uni-kassel.de>
2025-11-13 15:19:12 +0100

    add further examples

--- semi_supervised_example.py
+++ semi_supervised_example.py
@@ -1,81 +1,33 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.semi_supervised import LabelSpreading
 
 from skactiveml.classifier import SklearnClassifier
-from skactiveml.pool import UncertaintySampling
+from skactiveml.pool import TypiClust
 
-MISSING_LABEL = -1
 # Generate data set.
-X_all, y_true = make_blobs(
-    n_samples=800, centers=8, random_state=0, cluster_std=1.0
-)
-y_true = y_true % 4
+X, y_true = make_blobs(n_samples=800, centers=8)
+X, X_test, y_true, y_test = train_test_split(X, y_true, test_size=0.2)
+y = np.full(shape=y_true.shape, fill_value=-1)
+y[:10] = y_true[:10]
 
-plt.scatter(X_all[:, 0], X_all[:, 1], c=y_true)
-plt.show()
-
-X, X_test = X_all[:len(X_all)//2], X_all[len(X_all)//2:]
-y_true, y_true_test = y_true[:len(X_all)//2], y_true[len(X_all)//2:]
-
-
-class SSLClassifier(SklearnClassifier):
-    def __init__(
-        self,
-        ssl_model,
-        estimator,
-        classes=None,
-        missing_label=MISSING_LABEL,
-        cost_matrix=None,
-        random_state=None,
-    ):
-        super().__init__(
-            estimator=estimator,
-            missing_label=missing_label,
-            cost_matrix=cost_matrix,
-            random_state=random_state,
-            classes=classes,
-        )
-        self.ssl_model = ssl_model
-
-    def fit(self, X, y, **fit_kwargs):
-        self.ssl_model.fit(X, y)
-        y_ssl = self.ssl_model.predict(X)
-        return super().fit(X, y_ssl, **fit_kwargs)
-
-# Create classifiers and query strategy.
+# Create label propagation method, classifier and query strategy
+prop = LabelSpreading(gamma=1.0)
 clf = SklearnClassifier(
-    estimator=GaussianProcessClassifier(random_state=0),
-    classes=np.unique(y_true),
-    random_state=0,
-    missing_label=MISSING_LABEL,
+    GaussianProcessClassifier(),
+    missing_label=-1,
+    classes=np.unique(y_true)
 )
-clf_ssl = SSLClassifier(
-    LabelSpreading(kernel="rbf", gamma=1.0),
-    estimator=GaussianProcessClassifier(random_state=0),
-    classes=np.unique(y_true),
-    random_state=0,
-    missing_label=MISSING_LABEL,
-)
-clfs = [
-    clf,
-    clf_ssl,
-]
-for clf in clfs:
-    y = np.full(shape=y_true.shape, fill_value=MISSING_LABEL)
-    y[:10] = y_true[:10]
+qs = TypiClust(missing_label=-1)
 
-    qs = UncertaintySampling(method="margin_sampling", missing_label=MISSING_LABEL)
-    print(clf.__class__.__name__)
-    # Execute active learning cycle.
-    n_cycles = 20
-    for c in range(n_cycles):
-        clf.fit(X, y)
-        query_idx = qs.query(X=X, y=y, clf=clf, fit_clf=False)
-        y[query_idx] = y_true[query_idx]
-        print(np.mean(clf.predict(X_test) == y_true_test))
-
-    # Fit final classifier.
-    clf.fit(X, y)
+# Execute active learning cycle.
+n_cycles = 20
+scores = []
+for c in range(n_cycles):
+    query_idx = qs.query(X=X, y=y)
+    y[query_idx] = y_true[query_idx]
+    prop.fit(X, y)
+    clf.fit(X, prop.predict(X))
+    scores.append(clf.score(X_test, y_test))
